1c131faad2d35476436177b5c77b2cf42d316c9d
urgmaker <uranovrg@gmail.com>
2023-10-09 20:19:32 +0300

    Change PersonDetaisService

--- src/main/java/com/urgmaker/FirstSecurity/services/PersonDetailsService.java
+++ src/main/java/com/urgmaker/FirstSecurity/services/PersonDetailsService.java
@@ -2,13 +2,13 @@ package com.urgmaker.FirstSecurity.services;
 
 import com.urgmaker.FirstSecurity.models.Person;
 import com.urgmaker.FirstSecurity.repositories.PeopleRepository;
-import com.urgmaker.FirstSecurity.security.PersonDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -28,6 +28,10 @@ public class PersonDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User not found");
         }
 
-        return new PersonDetails(person.get());
+        return new org.springframework.security.core.userdetails.User(
+                person.get().getUsername(),
+                person.get().getPassword(),
+                Collections.emptyList()
+        );
     }
 }
