5d868e772d0afcdd504a13c11d0b40d1eac27a35
urgmaker <uranovrg@gmail.com>
2023-10-09 20:23:24 +0300

    Add daoAuthenticationProvider

--- src/main/java/com/urgmaker/FirstSecurity/config/SecurityConfig.java
+++ src/main/java/com/urgmaker/FirstSecurity/config/SecurityConfig.java
@@ -4,21 +4,35 @@ import com.urgmaker.FirstSecurity.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
 
     private final PersonDetailsService personDetailsService;
 
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(personDetailsService);
+        return daoAuthenticationProvider;
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
