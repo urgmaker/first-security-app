c647b9b941eabff199a91529c929819c96cb897a
r.uranov <uranovrg@gmail.com>
2023-10-11 09:20:11 +0300

    Add encodedPassword to register()

--- src/main/java/com/urgmaker/FirstSecurity/config/SecurityConfig.java
+++ src/main/java/com/urgmaker/FirstSecurity/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -61,6 +62,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/urgmaker/FirstSecurity/services/RegistrationService.java
+++ src/main/java/com/urgmaker/FirstSecurity/services/RegistrationService.java
@@ -3,18 +3,23 @@ package com.urgmaker.FirstSecurity.services;
 import com.urgmaker.FirstSecurity.models.Person;
 import com.urgmaker.FirstSecurity.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     @Transactional
     public void register(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
         peopleRepository.save(person);
     }
 }
