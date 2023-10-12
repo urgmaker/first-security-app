36e0aeb79bf46c00c2907467f8c9a75131d32004
r.uranov <uranovrg@gmail.com>
2023-10-12 09:23:33 +0300

    Update getAuthorities

--- src/main/java/com/urgmaker/FirstSecurity/security/PersonDetails.java
+++ src/main/java/com/urgmaker/FirstSecurity/security/PersonDetails.java
@@ -3,9 +3,11 @@ package com.urgmaker.FirstSecurity.security;
 import com.urgmaker.FirstSecurity.models.Person;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 public class PersonDetails implements UserDetails {
@@ -18,7 +20,7 @@ public class PersonDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(new SimpleGrantedAuthority(person.getRole()));
     }
 
     @Override
